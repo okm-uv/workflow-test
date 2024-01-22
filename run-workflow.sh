3f0031732b723d05388afd4a53ecbb1aef7fcd35
Masashi Okita <okita_masashi@uniquevision.co.jp>
2024-01-22 15:25:54 +0900

    force error

--- run-workflow.sh
+++ run-workflow.sh
@@ -1,21 +1,22 @@
 #! /usr/bin/sh -eu
 
 run_worklow() {
-  result=$(curl -vv \
+  workflow_list=$(curl --silent \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     -H 'Accept: application/vnd.github.everest-preview+json' \
     https://api.github.com/repos/okm-uv/workflow-test/actions/workflows)
 
-  echo "${result}"
-  id=$(echo "${result}" | jq -r ".workflows[0].id")
+  echo "${workflow_list}"
+  id=$(echo "${workflow_list}" | jq -r ".workflows[0].id")
   echo "$id"
 
-  curl \
-    -X POST \
+  result=$(curl --silent -X POST \
     -H 'Accept: application/vnd.github.v3+json' \
     -H "Authorization: token ${GITHUB_TOKEN}" \
     "https://api.github.com/repos/okm-uv/workflow-test/actions/workflows/${id}/dispatches" \
-    -d '{"ref":"main", "inputs": {"logLevel":"info","tags":false}}'
+    -d '{"ref":"main", "inputs": {"logLevel":"info","message":"test running workflow"}}')
+
+  echo "$result"
 }
 
 run_worklow "$@"
