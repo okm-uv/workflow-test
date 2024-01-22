f9303e1f53bc6f527ea46c941ccd5446acfec03d
Masashi Okita <okita_masashi@uniquevision.co.jp>
2024-01-22 16:32:00 +0900

    call with file-name

--- run-workflow.sh
+++ run-workflow.sh
@@ -1,19 +1,10 @@
-#! /usr/bin/sh -eu
+#!/bin/sh -eu
 
 run_worklow() {
-  workflow_list=$(curl --silent \
-    -H "Authorization: token ${GITHUB_TOKEN}" \
-    -H 'Accept: application/vnd.github.everest-preview+json' \
-    https://api.github.com/repos/okm-uv/workflow-test/actions/workflows)
-
-  echo "${workflow_list}"
-  id=$(echo "${workflow_list}" | jq -r ".workflows[0].id")
-  echo "$id"
-
   result=$(curl --silent -X POST \
     -H 'Accept: application/vnd.github.v3+json' \
     -H "Authorization: token ${GITHUB_TOKEN}" \
-    "https://api.github.com/repos/okm-uv/workflow-test/actions/workflows/${id}/dispatches" \
+    "https://api.github.com/repos/okm-uv/workflow-test/actions/workflows/main.yaml/dispatches" \
     -d '{"ref":"main", "inputs": {"logLevel":"info","message":"test running workflow"}}')
 
   echo "$result"
